7c660cad77ec91a08ad8423b0b16959aee6f59e0
Vicky AS <vickyhuang808@gmail.com>
2024-04-09 10:02:39 +0700

    make contact model

---
+++ database/migrations/2024_04_09_024305_create_contacts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name',100)->nullable(false);
+            $table->string('last_name',100)->nullable();
+            $table->string('email',200)->nullable();
+            $table->string('phone',20)->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
