9ad3d8b8a8919d330428fca17b407935761c9fbc
Vicky AS <vickyhuang808@gmail.com>
2024-05-03 19:14:01 +0700

    contact data update

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactCreateRequest;
+use App\Http\Requests\ContactUpdateRequest;
 use App\Http\Resources\ContactResource;
 use App\Models\Contact;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -28,17 +29,38 @@ class ContactsController extends Controller
     {
         $user = Auth::user();
 
-        $contact = Contact::where('id',$id)->where('user_id',$user->id)->first();
+        $contact = Contact::where('id', $id)->where('user_id', $user->id)->first();
 
         if (!$contact) {
             throw new HttpResponseException(response()->json([
-                'errors'=>[
-                    "message" =>[
+                'errors' => [
+                    "message" => [
                         "not found"
                     ]
                 ]
             ])->setStatusCode(404));
         }
+
+        return new ContactResource($contact);
+    }
+
+    public function update(int $id, ContactUpdateRequest $request): ContactResource
+    {
+        $user = Auth::user();
+        $contact = Contact::where('id', $id)->where('user_id', $user->id)->first();
+        if (!$contact) {
+            throw new HttpResponseException(response()->json([
+                'errors' => [
+                    "message" => [
+                        "not found"
+                    ]
+                ]
+            ])->setStatusCode(404));
+        }
+
+        $data = $request->validated();
+        $contact->fill($data);
+        $contact->save();
 
         return new ContactResource($contact);
     }
