0a6e046084a261843df15586eb07ecc35ea3605c
Vicky AS <vickyhuang808@gmail.com>
2024-05-04 12:24:38 +0700

    get address data

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\AddressResource;
 use App\Http\Resources\ContactResource;
 use App\Models\Address;
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -14,9 +15,9 @@ use Illuminate\Support\Facades\Auth;
 
 class AddressController extends Controller
 {
-    public function create(int $idContact, AddressCreateRequest $request): JsonResponse
-    {
-        $user = Auth::user();
+
+
+    private function getContact(User $user,int $idContact):Contact{
         $contact = Contact::where('user_id', $user->id)->where('id', $idContact)->first();
 
         if (!$contact) {
@@ -29,11 +30,43 @@ class AddressController extends Controller
             ])->setStatusCode(404));
         }
 
+        return $contact;
+    }
+
+    private function getAddress(Contact $contact,int $idAddress):Address{
+        $address = Address::where('contact_id',$contact->id)->where('id',$idAddress)->first();
+
+        if (!$address) {
+            throw new HttpResponseException(response()->json([
+                'errors' => [
+                    "message" => [
+                        "not found"
+                    ]
+                ]
+            ])->setStatusCode(404));
+        }
+        return $address;
+    }
+
+
+    public function create(int $idContact, AddressCreateRequest $request): JsonResponse
+    {
+        $user = Auth::user();
+        $contact= $this->getContact($user,$idContact);
+
         $data = $request->validated();
         $address = new Address($data);
         $address->contact_id = $contact->id;
         $address->save();
 
         return (new AddressResource($address))->response()->setStatusCode(201);
+    }
+
+    public function get(int $idContact,int $idAddress):AddressResource{
+        $user = Auth::user();
+        $contact = $this->getContact($user, $idContact);
+        $address = $this->getAddress($contact, $idAddress);
+
+        return new AddressResource($address);
     }
 }
